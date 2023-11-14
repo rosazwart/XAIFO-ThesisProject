c82303916729e510f5cc081fafebd3294b4a036d
Rosa Zwart <rosa-zwart@hotmail.com>
2023-11-14 01:45:06 +0100

    added additional experiments g2

--- run_notebooks.py
+++ run_notebooks.py
@@ -3,7 +3,7 @@ import subprocess
 if __name__ == "__main__":
     notebooks_to_run = ['edge2vec_embedding.ipynb', 'predictor.ipynb']  # Replace with your notebook file names
 
-    for i in range(10):
+    for i in range(9):
         print(f'Time {i}')
         for notebook in notebooks_to_run:
             cmd = f'jupyter nbconvert --execute --inplace {notebook}'
