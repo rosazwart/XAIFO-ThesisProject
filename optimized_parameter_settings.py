beb39173fcab31570bef5aed3a046e4a650731d1
rosazwart <rosa-zwart@hotmail.com>
2024-07-11 17:13:44 +0200

    Added results for hyperparam optimization

---
+++ optimized_parameter_settings.py
@@ -0,0 +1,34 @@
+{
+    'prev': {
+        'hidden_dim': 64,
+        'output_dim': 64,
+        'epochs': 200,
+        'type_size': 22,
+        'epoch_e2v': 10,
+        'num_walks': 4,
+        'walk_length': 5,
+        'p': 1,
+        'q': 0.5,
+        'dimensions_e2v': 64,
+        'lr': 0.0,
+        'aggr': 'mean',
+        'dropout': 0,
+        'layers': 6
+    },
+    'restr': {
+        'hidden_dim': 0,
+        'output_dim': 0,
+        'epochs': 0,
+        'type_size': 0,
+        'epoch_e2v': 0,
+        'num_walks': 0,
+        'walk_length': 0,
+        'p': 0,
+        'q': 0,
+        'dimensions_e2v': 0,
+        'lr': 0,
+        'aggr': '',
+        'dropout': 0,
+        'layers': 0
+    }
+}
