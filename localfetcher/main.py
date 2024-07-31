b370445491126e2685e3dda6f97ff63d015f4ced
Rosa Zwart <rosa-zwart@hotmail.com>
2024-07-31 13:58:42 +0200

    Added seeds for osteogenesis imperfecta

--- localfetcher/main.py
+++ localfetcher/main.py
@@ -4,20 +4,40 @@ from association_getter import get_monarch_associations
 
 if __name__ == "__main__":
     all_assoc_df = get_all_assoc_df(dir_name='localfetcher/deprecated_data')
+    
+    disease_prefix = input('For which disease wil the knowledge graph be built? Choose from "dmd", "hd" and "oi"')
+    assert disease_prefix == 'dmd' or 'hd' or 'oi'
+    
+    if disease_prefix == 'hd':
+        # HD Seeds
 
-    # HD Seeds
+        seeds = [
+            'MONDO:0007739',    # Huntington disease
+            'HGNC:4851' # HTT, causal gene Huntington disease
+        ]
+        
+    elif disease_prefix == 'dmd':
 
-    #seeds = [
-    #    'MONDO:0007739',    # Huntington disease
-    #    'HGNC:4851' # HTT, causal gene Huntington disease
-    #]
+        # DMD Seeds
 
-    # DMD Seeds
-
-    seeds = [
-        'MONDO:0010679',
-        'HGNC:2928'
+        seeds = [
+            'MONDO:0010679',    # Duchenne muscular dystrophy disease
+            'HGNC:2928'         # DMD, causal gene
+        ]
+    
+    elif disease_prefix == 'oi':
+    
+        # OI Seeds
+        
+        seeds = [
+            'MONDO:0019019',    # Osteogenesis imperfecta
+            'HGNC:2197',        # COL1A1, causal gene
+            'HGNC:2198'         # COL1A2, causal gene
     ]
+        
+    else:
+        
+        seeds = []
 
     all_assoc = get_monarch_associations(assoc_df=all_assoc_df, nodes_list=seeds,
-                                         disease_file_name_ref='dmd')
+                                         disease_file_name_ref=disease_prefix)
